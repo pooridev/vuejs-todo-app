b4e6cd71a06366610f76120f1e53603d3c75453f
Pooria Faramarzian <PooriaFaramarzian@gmail.com>
2021-09-19 20:43:46 +0430

    [feat] default prop

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header @click="logProp">
+    <h1>
+      {{ title }}
+    </h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: 'Hello Vue!'
+    }
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
